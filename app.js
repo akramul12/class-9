4afe32c40f1645cc810a3c10a9e3bc04dc198ae9
akramul12 <rexakramulking@gmail.com>
2022-05-11 19:48:36 +0600

    uplod my assirment 9

---
+++ app.js
@@ -0,0 +1,39 @@
+const deves = [
+    {
+        id : 1,
+        nmae : "Akramul islam",
+        age : 10,
+        skill : "Wordpress Developer"
+    },
+    {
+        id : 2,
+        name : "shakil islam",
+        age : 20,
+        skill : "Iso developer"
+
+    },
+    {
+        id : 3,
+        name : "shakil islam",
+        age : 20,
+        skill : "Iso developer"
+
+    },
+    {
+        id : 4,
+        name : "shakil islam",
+        age : 20,
+        skill : "Iso developer"
+
+    },
+    {
+        id : 5,
+        name : "shakil islam",
+        age : 20,
+        skill : "Iso developer"
+
+    },
+]
+
+console.log(JSON.stringify(deves));
+
